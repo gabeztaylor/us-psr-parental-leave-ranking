aad6e41c6398fdf50bbf032049894a753f168f53
Gabe Taylor <gabe.z.taylor@gmail.com>
2026-03-10 09:05:18 -0500

    change base url

--- vite.config.jsx
+++ vite.config.jsx
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/us-psr-parental-leave-ranking/",   // ← must match your GitHub repo name exactly
+//   base: "/us-psr-parental-leave-ranking/",   
+  base: "/",
 });
