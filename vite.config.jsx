f2e7ab6e590f0c81ae8e4ac5d35cc8a7c15bb1d1
Gabe Taylor <gabe.z.taylor@gmail.com>
2026-03-10 09:11:11 -0500

    change base back again

--- vite.config.jsx
+++ vite.config.jsx
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 //   base: "/us-psr-parental-leave-ranking/",   
-  base: "/",
+  base: "/us-psr-parental-leave-ranking/",
 });
